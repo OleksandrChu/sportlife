8c6419b62beabe857f8a6a2a1a762f672a8013a9
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-09-04 09:23:44 +0300

    Create ClientService class

---
+++ Services/ClientService.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using sportlife.Data;
+using sportlife.Models;
+
+namespace sportlife.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public ClientService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Client> Create(Client client)
+        {
+            _context.Add(client);
+            await _context.SaveChangesAsync();
+            return client;
+        }
+
+        public async Task<Client> Select(int id)
+        {
+            var client = await _context.Clients
+                .Include(membership => membership.MemberShip)
+                .Where(clientId => clientId.Id == id).FirstAsync();
+            return client;
+        }
+    }
+}
