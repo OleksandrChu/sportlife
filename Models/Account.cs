7068abe94b4ee093a589b357e1887a2ea1b04261
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-09-06 22:02:50 +0300

    exclude property

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace sportlife.Models
 {
     public class Account
     {
         public int Id { get; set; }
+        [NotMapped]
         public int Balance { get; set; }
         public List<Transaction> Transactions { get; set; }
     }
