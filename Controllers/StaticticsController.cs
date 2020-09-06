9df196d7e340c1abf6b2664e86edf2511cd67eea
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-09-07 00:39:55 +0300

    Create statistics controller

---
+++ Controllers/StaticticsController.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sportlife.Data;
+using sportlife.Models;
+using sportlife.Services;
+
+namespace sportlife.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StaticticsController : ControllerBase
+    {
+        private readonly IStaticticsService _statisticsService;
+
+        public StaticticsController(IStaticticsService staticticsService)
+        {
+            _statisticsService = staticticsService;
+        }
+
+        [HttpGet]
+        public ActionResult GetStat() 
+        {
+            // SELECT type, COUNT(type) FROM Memberships GROUP BY type
+            return Ok(_statisticsService.GetByType());
+        }
+    }
+}
